68701180b37c02d263d58a5c6e978ce43b23ee87
Jrios09 <juli.rios08@gmail.com>
2025-04-02 20:26:46 -0500

    Configuracion variables de entorno en el proyecto

--- workout-tracker/src/app.js
+++ workout-tracker/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const { port } = require('./config/env'); 
 const app = express(); 
-const port = 8000; 
+
   
 app.get("/", (req, res) => {
     res.send("Hola mi server en Express");
